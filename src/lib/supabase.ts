a45214177ee8a43952d1f3c359bb4456322dc507
thakuramit79 <thakuramit79@gmail.com>
2025-05-28 07:47:44 +0530

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mihhehjdoyncyfczefnv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1paGhlaGpkb3luY3lmY3plZm52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzMzk5NDAsImV4cCI6MjA2MzkxNTk0MH0.GuMABzSXxIu7q3hTu6mMEnikiRoFha4BTnM2zxoBCt8';
+const supabaseUrl = VITE_SUPABASE_URL;
+const supabaseAnonKey = VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
